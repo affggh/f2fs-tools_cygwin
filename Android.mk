17705ce0090320f733a189bfce84430ae1ba6915
Jaegeuk Kim <jaegeuk@google.com>
2018-08-20 13:39:27 -0700

    [DO NOT MERGE] make_f2fs: issue discard commands on mkfs
    We'd better to discard whole partition for better performance.
    
    Bug: 112868088
    Test: factory reset
    Change-Id: I9c0c711f2b081e4d0b4e145ab08f95b382925847
    Merged-In: I9c0c711f2b081e4d0b4e145ab08f95b382925847
    Signed-off-by: Jaegeuk Kim <jaegeuk@google.com>

--- Android.mk
+++ Android.mk
@@ -4,7 +4,7 @@ LOCAL_PATH:= $(call my-dir)
 ifeq ($(HOST_OS),linux)
 
 # The versions depend on $(LOCAL_PATH)/VERSION
-version_CFLAGS := -DF2FS_MAJOR_VERSION=1 -DF2FS_MINOR_VERSION=4 -DF2FS_TOOLS_VERSION=\"1.4.0\" -DF2FS_TOOLS_DATE=\"2014-10-18\"
+version_CFLAGS := -DF2FS_MAJOR_VERSION=1 -DF2FS_MINOR_VERSION=4 -DF2FS_TOOLS_VERSION=\"1.4.0\" -DF2FS_TOOLS_DATE=\"2014-10-18\" -DWITH_BLKDISCARD
 
 # external/e2fsprogs/lib is needed for uuid/uuid.h
 common_C_INCLUDES := $(LOCAL_PATH)/include external/e2fsprogs/lib/
