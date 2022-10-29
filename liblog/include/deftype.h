9db374a4e0a858e54a84b2555fac65c8d67e6dfe
xiaoxindada <2245062854@qq.com>
2022-10-29 22:26:45 +0800

    import liblog
    Signed-off-by: xiaoxindada <2245062854@qq.com>

---
+++ liblog/include/deftype.h
@@ -0,0 +1,6 @@
+// Prefix type _Bool by affggh
+
+#ifdef __CYGWIN__
+#include <stdbool.h>
+typedef bool _Bool;
+#endif
---
+++ liblog/myboolean.h
@@ -0,0 +1,8 @@
+#ifndef MYBOOLEAN_H
+#define MYBOOLEAN_H
+
+#define false 0
+#define true 1
+typedef int bool; // or #define bool int
+
+#endif
